ee435f89a14e37cef16f36f98b7ecc00bc21f414
Leandro Marques <leandroppmarques@hotmail.com>
2023-04-03 16:02:33 -0300

    ajuste

---
+++ src/components/Message.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="message-container">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+  .message-container {
+    color: #004085;
+    background-color: #CCe5FF;
+    border-radius: 2px solid #B8DAFF;
+    padding: 10px;
+    max-width: 400px;
+    margin: 30px auto;
+  }
+</style>
